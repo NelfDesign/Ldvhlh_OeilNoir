ebf5feb3c83c3ff5eab6db327f94454c43220af2
LEGER Fabrice <fabricedesign@MBP-de-LEGER.home>
2020-06-09 10:19:49 +0200

    add jet property

--- app/src/main/java/fr/fabriceDesign/ldvhlh_loeilnoir/model/Page.kt
+++ app/src/main/java/fr/fabriceDesign/ldvhlh_loeilnoir/model/Page.kt
@@ -9,16 +9,47 @@ import com.google.gson.annotations.SerializedName
 data class Page(@SerializedName("number") var number:Int,
                 @SerializedName("text") var text : String,
                 @SerializedName("choix") var choix : ArrayList<Choix>,
-                @SerializedName("ennemis") var ennemi : ArrayList<Ennemi>?)
+                @SerializedName("ennemis") var ennemi : ArrayList<Ennemi>?,
+                @SerializedName("jet") var jet : ArrayList<Jet>?,
+                @SerializedName("bonus") var bonus : ArrayList<Bonus>?,
+                @SerializedName("condition") var condition : ArrayList<ConditionVie>?
+)
+
+data class ConditionVie (@SerializedName("type") var type: String,
+                         @SerializedName("minimum") var minimum : String,
+                         @SerializedName("number") var number: Int
+)
+
+data class Bonus (@SerializedName("type") var type: String,
+                  @SerializedName("modif") var modif: String,
+                  @SerializedName("number") var number: Int
+)
+
+data class Jet (@SerializedName("type") var type  : String,
+                @SerializedName("plus") var plus : Int?,
+                @SerializedName("moins") var moins : Int?,
+                @SerializedName("multiplier") var multi : Int?,
+                @SerializedName("de") var de : Int,
+                @SerializedName("number") var number : Int?,
+                @SerializedName("capa") var capa : String?
+)
 
 data class Choix (@SerializedName("text") val text : String?,
-                  @SerializedName("number") val number : Int)
+                  @SerializedName("number") var number : Int?
+)
 
 data class Ennemi(@SerializedName("name") var name: String,
+                  @SerializedName("combat") var combat: String,
+                  @SerializedName("age") var age: Int?,
                   @SerializedName("courage") var courage : Int,
-                  @SerializedName("force") var force : Int,
-                  @SerializedName("intel") var intel : Int,
-                  @SerializedName("charisme") var charisme : Int,
-                  @SerializedName("adresse") var adresse : Int,
-                  @SerializedName("vie") var vie : Int,
-                  @SerializedName("aventure") var aventure : Int = 0)
+                  @SerializedName("energie") var energie : Int,
+                  @SerializedName("protection") var protection : Int,
+                  @SerializedName("attaque") var attaque : Int,
+                  @SerializedName("parade") var parade : Int,
+                  @SerializedName("impact") var impact : ArrayList<Impact>,
+                  @SerializedName("aventure") var aventure : Int = 0
+)
+
+class Impact (@SerializedName("de") var de: Int,
+              @SerializedName("plus") var plus : Int
+)
--- app/src/main/java/fr/fabriceDesign/ldvhlh_loeilnoir/ui/fragments/ui/game/GameViewModel.kt
+++ app/src/main/java/fr/fabriceDesign/ldvhlh_loeilnoir/ui/fragments/ui/game/GameViewModel.kt
@@ -1,31 +1,57 @@
 package fr.fabriceDesign.ldvhlh_loeilnoir.ui.fragments.ui.game
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import fr.fabriceDesign.ldvhlh_loeilnoir.app.App
 import fr.fabriceDesign.ldvhlh_loeilnoir.model.Page
-import fr.fabriceDesign.ldvhlh_loeilnoir.ui.activity.Game.Companion.PAGE_NUMBER
+import fr.fabriceDesign.ldvhlh_loeilnoir.model.Personnage
+import fr.fabriceDesign.ldvhlh_loeilnoir.ui.activity.Game.Companion.NAME_PERSO
+import java.security.SecureRandom
 import javax.inject.Inject
 
 class GameViewModel @Inject constructor() : ViewModel() {
 
-    fun setNumberPage(number : Int){
-        PAGE_NUMBER = number
-    }
-    fun getNumberPage() : Int {
-        return PAGE_NUMBER
-    }
+    var perso: LiveData<Personnage> = App.database.personnageDao().getPersonnageByName(NAME_PERSO!!)
 
-    fun parseJson(json : String) : MutableList<Page>{
+    fun parseJson(json: String): MutableList<Page> {
         val pagesLivres = mutableListOf<Page>()
         //permet de connaitre le type de retour de la liste
         val listType = object : TypeToken<ArrayList<Page>>() {}.type
         //on crée un tableau de pages
-        val pages= Gson().fromJson<ArrayList<Page>>(json, listType)
+        val pages = Gson().fromJson<ArrayList<Page>>(json, listType)
         for (page in pages) {
             pagesLivres.add(page)
         }
         return pagesLivres
+    }
+
+    fun updateUi(capa: String, capaPerso: Int, result: Int, resultKeep: Int, texte: String): String {
+        val text = StringBuilder()
+        text.append("Votre $capa = $capaPerso.")
+        text.append("\nRésultat du lancé de dé : $result")
+        text.append("\nRésultat du dé après modification : $resultKeep")
+        text.append("\n$texte")
+        return text.toString()
+    }
+
+    fun updateChoice(result: Int, texte: String): String {
+        val text = StringBuilder()
+        text.append("\nRésultat du lancé de dé : $result")
+        text.append("\n$texte")
+        return text.toString()
+    }
+
+
+    fun diceJet6(): Int {
+        val rand = SecureRandom()
+        return rand.nextInt(6) + 1
+    }
+
+    fun diceJet20(): Int {
+        val rand = SecureRandom()
+        return rand.nextInt(20) + 1
     }
 
 }
